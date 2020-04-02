8893d2cb27a33ff04b316024738d2f30e4cc412f
Guy rahamim <guy.danino5@gmail.com>
2020-04-02 18:27:37 +0300

    Lecture 54
    Move object with mouse!

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Paddle : MonoBehaviour
+{
+	float mouseXPoisition, mouseYPosition;
+	[SerializeField] float screenWidthInUnits= 16f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Debug.Log(Input.anyKeyDown.ToString());
+		mouseXPoisition = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+		mouseYPosition = transform.position.y;
+		Vector2 paddlePosition = new Vector2(mouseXPoisition,mouseYPosition);
+		transform.position = paddlePosition;
+
+    }
+}
